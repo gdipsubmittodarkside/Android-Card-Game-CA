8b2ce9610e994cd39da9caca6b4ba10342f4684d
Lycanx4 <51407306+Lycanx4@users.noreply.github.com>
2023-01-04 10:51:04 +0800

    trash

--- app/src/main/java/iss/workshop/android_ca/MainActivity.java
+++ app/src/main/java/iss/workshop/android_ca/MainActivity.java
@@ -1,24 +1,36 @@
 package iss.workshop.android_ca;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +40,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -35,29 +48,38 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity implements RecyclerViewInterface {
     EditText mURL; // where URL is placed
     RecyclerView recyclerView;
+    TextView textView;
     Button mFetchBtn;
+    Button mLeaderBoardBtn;
     File dir;
     String url; // input URL (stocksnap.io)
+    File imageURLsFileDir; // not sure what this is for. seems like not used
     List <String> imgURLS = new ArrayList<>(); // list of 20 image Urls from website (eg stocksnap.io)
 
     List<Uri> uri; // list of 20 Uri referring to 20 jpg images in external storage
     HashMap<String, Integer> leaderBoard;
 
     // for placement of first 20 blank
-//    Uri dummy = Uri.parse("https://cdn-icons-png.flaticon.com/512/59/59836.png");
-//    List<Uri> dummies = new ArrayList<>();
+    Uri dummy = Uri.parse("https://cdn-icons-png.flaticon.com/512/59/59836.png");
+    List<Uri> dummies = new ArrayList<>();
 
     RecyclerAdapter adapter;
     Context context;
     ProgressBar progressBar;
     TextView downloadProgress;
+    private  static  final  int Read_Permission = 101;
     int currentProgress = 0;
 
     // YT added
@@ -68,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mLeaderBoardBtn;
+
         mURL = findViewById(R.id.urlTxt);
         recyclerView = findViewById(R.id.recyclerView);
         progressBar = findViewById(R.id.progressBar);
@@ -76,27 +98,31 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
         dir =cw.getDir("imageDir",Context.MODE_PRIVATE);
 
-
-            uri = LoadImageUri();
+        uri = LoadImageUri();
 
 //        // Set layout
 //        for(int i=0; i<20;i++){
 //            dummies.add(dummy);
 //        }
-            if (uri.size() == 20) {
-                adapter = new RecyclerAdapter((ArrayList<Uri>) uri, this);
-            }
+        if(uri.size()==20){
+            adapter = new RecyclerAdapter((ArrayList<Uri>) uri, this);        }
 
 
             // set 5 x 4
-            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 4));
-            recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 4));
+        recyclerView.setAdapter(adapter);
 
-            // an empty thread. not sure what this is for?
-            HomePage hm = new HomePage();
-            hm.execute();
-            context = this;
+        // an empty thread. not sure what this is for?
+        HomePage hm = new HomePage();
+        hm.execute();
+        context = this;
 
+        // give permission to access external storage
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Read_Permission);
+        }
 
         // listening for fetch button to start download
         mFetchBtn = findViewById(R.id.btnFetch);
@@ -124,164 +150,16 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
             }
         });
 
-
-        leaderBoard = loadLeaderBoard();
-
-        mLeaderBoardBtn = findViewById(R.id.leaderBoard_button);
-
-        if(leaderBoard == null){
+        leaderBoard = LeaderBoard.loadLeaderBoard();
+        if(leaderBoard.isEmpty()){
             mLeaderBoardBtn.setEnabled(false);
         }
-        if(leaderBoard!=null){
-            mLeaderBoardBtn.setEnabled(true);
-        }
-        mLeaderBoardBtn.setOnClickListener(view -> startleaderBoard(leaderBoard));
-
-        // to set first download progress?
-        if(mURL != null){
-            SharedPreferences pref =
-                    getPreferences(Context.MODE_PRIVATE);
-            int download = pref.getInt("downloadProgress",0);
-            downloadProgress.setText("Download " +download+ " of 20 images");
-
-        }
-
-        // confirm 6 images button
-        cfmBtn = findViewById(R.id.cfm6_button);
-        cfmBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                confirmBtnClicked();
-            }
-        });
-    }
-
-    private void startleaderBoard(HashMap<String, Integer> scoreList) {
-        ArrayList<String> playerNames = new ArrayList<>(scoreList.keySet());
-        ArrayList<Integer> playerScores = new ArrayList<>(scoreList.values());
-
-        Intent intent = new Intent(this, LeaderBoard.class);
-        intent.putStringArrayListExtra("names", playerNames);
-        intent.putIntegerArrayListExtra("scores", playerScores);
-        startActivity(intent);
-    }
-
-    public class HomePage extends AsyncTask<Void, Void, Void>{
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
-        }
-    }
-
-    // Main goal: put 20 image url into global attr imageURLs : List<String>
-    public class WebScrape extends AsyncTask<Void,Void,Void>{
-
-        // before download
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-                progressBar.setVisibility(View.VISIBLE);
-                downloadProgress.setVisibility(View.VISIBLE);
-                downloadProgress.setText("Downloading");
-        }
-
-        // after 20 images have finished downloading
-        @Override
-        protected void onPostExecute(Void unused) {
-            super.onPostExecute(unused);
-            progressBar.setVisibility(View.GONE);
-            progressBar.startAnimation
-                    (AnimationUtils.loadAnimation
-                            (MainActivity.this,
-                                    android.R.anim.fade_out));
-            downloadProgress.setText("Download Finished");
-            adapter.notifyDataSetChanged();
-
-            //DIalog to go to another page
-            /*
-            AlertDialog.Builder alertBialogBuilder = new AlertDialog.Builder(MainActivity.this);
-            alertBialogBuilder
-                    .setMessage(">>>>Ready to Play<<<<")
-                    .setCancelable(false)
-                    .setPositiveButton("Play", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent  = new Intent(MainActivity.this, GamePlaying.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    })
-                    .setNegativeButton("CHOOSE AGAIN", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-            AlertDialog alertDialog = alertBialogBuilder.create();
-            alertDialog.show();*/
-
-
-        }
-
-        // webscrape 20 image urls and add to imageURLs
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Document document = null;
-            try {
-                if(url == null){
-
-                }
-                else{
-                document = Jsoup.connect(url).get();
-                Elements images = document.select("img[src~=(?i)\\.(png|jpe?g|gif|webp)]");
-                int i = 0;
-
-                for (Element image : images) {
-//                    String imageUrl = image.attr("data-original");
-                    String imageUrl = image.absUrl("src");
-
-
-                    if(imageUrl.startsWith("https")){
-                        // gets list of 20 image urls
-                        imgURLS.add(imageUrl);
-
-                        startDownloadImage(imageUrl, i+1);
-
-                        // progress bar
-                        currentProgress += 5;
-                        progressBar.setProgress(currentProgress);
-                        progressBar.setMax(100);
-                        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sp.edit();
-                        editor.putInt("downloadProgress",i);
-                        i++;
-                    }
-
-                    if(i>=20){
-                        break;
-                    }}
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                Toast.makeText(
-                        MainActivity.this,
-                        e.getMessage(),
-                        Toast.LENGTH_SHORT)
-                        .show();
-            }
-            return null;
-        }
-    }
-
-    // used in "WebScrape" class
+        if(leaderBoard.isEmpty()){
     // Goal: for each image,
     // 1) download from URL -> byte[]
     // 2) store in external folder "app_imageDir -> jpg
     // 3) set image into recyclerView
-    protected void startDownloadImage( String imgURL, int num){
+    protected void startDownloadImage(String imgURL, int num){
         String destFilename =
                 String.valueOf(num) + imgURL.lastIndexOf(".")+1;
 
@@ -321,19 +199,17 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
     protected ArrayList<Uri> LoadImageUri(){
         ArrayList<Uri> loadImageuris = new ArrayList<>();
 
-            for (int i = 1; i <= 20; i++) {
-            File imageFile = new File(dir, "Img" + i + ".jpg");
-            Uri uriImg = Uri.fromFile(imageFile);
+        for(int i = 1; i<=20; i++){
+            File imageFile = new File(dir, "Img" + String.valueOf(i)+".jpg");
+            Uri uriImg= Uri.fromFile(imageFile);
             loadImageuris.add(uriImg);
-            }
+        }
         return loadImageuris;
-
-
     }
 
     // byte[] --> jpg file. Returns Uri of the jpg image
     protected Uri SaveImageJPG(int num, Bitmap bitmap){
-        File imageFile = new File(dir, "Img" + num+ ".jpg");
+        File imageFile = new File(dir, "Img" + String.valueOf(num)+".jpg");
         FileOutputStream out = null;
         try{
             out = new FileOutputStream(imageFile);
@@ -354,24 +230,19 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
 
     // for selecting 6 images
     @Override
-    public void onImageClick(int position, View view){
+    public void onImageClick(int position){
         Uri selected_imgUri = uri.get(position);
 
         if (selected_6Uri.size() < 6 && !selected_6Uri.contains(selected_imgUri)){
-            ConstraintLayout image_container = (ConstraintLayout) view.getParent();
-            image_container.setBackgroundColor(Color.parseColor("#FF0000")); // red
-
             selected_6Uri.add(selected_imgUri);
         }
         else if (selected_6Uri.contains(selected_imgUri)){
-            ConstraintLayout image_container = (ConstraintLayout) view.getParent();
-            image_container.setBackgroundColor(Color.parseColor("#FFFFFF")); // white
-
             selected_6Uri.remove(selected_imgUri);
         }
         else{
             Toast.makeText(this,"Select 6 only",Toast.LENGTH_SHORT).show();
         }
+        recyclerView.setAdapter(adapter);
 
         // Enable or disable "confirm" button
         Button btnSelect = findViewById(R.id.cfm6_button);
@@ -441,27 +312,16 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
             startActivity(intent);
     }
 
-    public HashMap<String,Integer> loadLeaderBoard(){
-        HashMap<String,Integer> scoreList = new HashMap<>();
-        File lbs = getLeaderBoardFile();
-        try
-        {
-            FileInputStream fileInputStream  = new FileInputStream(lbs);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
-            scoreList = (HashMap) objectInputStream.readObject();
-            objectInputStream.close();
-            return scoreList;
-        }
-        catch(ClassNotFoundException | IOException | ClassCastException e) {
-            e.printStackTrace();
-        }
-        return scoreList;
-    }
 
-    public File getLeaderBoardFile(){
-        String filePath = "LBfolder";
-        String fileName = "LBS.txt";
-        return new File(getFilesDir(),  filePath + "/" + fileName);
+    protected void startleaderBoard(HashMap<String,Integer> scoreList){
+
+        ArrayList<String> playerNames = new ArrayList<>(scoreList.keySet());
+        ArrayList<Integer> playerScores = new ArrayList<>(scoreList.values());
+
+        Intent intent = new Intent(this, LeaderBoard.class);
+        intent.putStringArrayListExtra("names", playerNames);
+        intent.putIntegerArrayListExtra("scores", playerScores);
+        startActivity(intent);
     }
 }
